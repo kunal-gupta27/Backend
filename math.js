595982876ffaa11c61453dc0173d5083575a23b2
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2026-02-09 20:26:45 +0530

    learning backend

---
+++ math.js
@@ -0,0 +1,41 @@
+// module.exports.sum = (a, b) => a+b;
+// module.exports.mul = (a, b) => a*b;
+// module.exports.g = 9.8;
+// module.exports.pi = 3.14;
+
+//++++++++++++++++ OR++++++++++++
+
+
+// exports.sum = (a, b) => a+b;
+// exports.mul = (a, b) => a*b;
+// exports.g = 9.8;
+// exports.pi = 3.14;
+
+//++++++++++++ OR+++++++++++++++++++
+
+// export = 5 //aesa nhi kr sakte 
+
+
+// module.exports= 123; 
+// let obj = {
+//     sum: sum,
+//     mul:mul,
+//     g:g,
+//     pi:pi,
+// };
+
+// module.exports= obj; 
+
+// module.exports = {
+//     sum: sum,
+//     mul:mul,
+//     g:g,
+//     pi:pi,
+// };
+
+
+
+export const sum = (a, b) => a+b;
+export const mul = (a, b) => a*b;
+export const g = 9.8;
+export const pi = 3.14;
---
+++ script.js
@@ -0,0 +1,41 @@
+// let n = 5
+
+// for(let i=0;i<n;i++){
+//     console.log("hello", i);
+    
+// }
+// console.log("bye");
+
+// let args = process.argv;
+// for(let i=0;i<args.length;i++){
+//     console.log(("hell0 & welcome to ", args[i]));
+    
+// }
+// console.log(process.argv);
+
+// const someValue = require("./math");
+
+// console.log(someValue);
+
+// const math = require("./math")
+
+// console.log(math.sum(2, 3));
+// console.log(math.pi);
+// console.log(math.mul(2,3));
+
+
+
+// for directory
+
+// const info = require("./fruits")
+
+// console.log(info[0].name);
+
+
+import{sum, pi} from "./math.js";
+import { generate, count } from "random-words";
+
+
+// console.log(sum(1, 2));
+// console.log(pi);
+console.log(generate());
