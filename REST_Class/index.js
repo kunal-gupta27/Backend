3fda2fc57fec7d4989fb0587b91ad8acfbea87cb
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2026-02-24 23:44:38 +0530

    learning rest_api and uuid

--- REST_Class/index.js
+++ REST_Class/index.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express();
 const port = 8080;
 const path = require("path");
+const  {v4: uuidv4} = require('uuid');
+
+
 
 app.use(express.urlencoded({extended : true}));
 
@@ -12,14 +15,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
+        id: uuidv4(),
         username: "apnacollege",
         content: "i love coding"
     },
     {
+        id: uuidv4(),
         username: "kunal gupta",
         content: "hard work is important for successfull carrer"
     },
     {
+        id: uuidv4(),
         username: "priyanshu kumar",
         content: "i love gaming"
     },
@@ -27,11 +33,25 @@ let posts = [
 
 
 app.get("/posts", (req,res) => {
-    res.render("index.ejs", {posts});
+    let {username, content} = req.body;
+
+    posts.push({
+        id: uuidv4(),
+        username,
+        content
+    })
+    res.render("/posts");
 })
 
 app.get("/posts/new", (req, res)=> {
     res.render("new.ejs");
+})
+
+app.patch("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    console.log(id);
+    res.send("patch req working");
+    
 })
 
 app.post("/posts",(req,res) => {
@@ -39,6 +59,16 @@ app.post("/posts",(req,res) => {
     posts.push({username, content})
     res.redirect("/posts")   
 })
+
+app.get("/posts/:id", (req, res)=> {
+    let {id} = req.params;
+    let post = posts.find((p) =>(id === p.id))
+    console.log(id);
+    // res.send("request working");
+    res.render("show.ejs", {post})
+    
+})
+
 app.listen(port, () => {
     console.log("listening to port : 8080");
     
